cae003b072c35824d4723a585e307fb2c37007fd
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-08 20:12:41 +0000

    cssPath + xPath

--- ClassNamer/ClassNames.cs
+++ ClassNamer/ClassNames.cs
@@ -4,9 +4,17 @@ using System;
 
 class ClassName
 {
+    /* Quick Note!
+     * In inspector using "Copy Selector" gives you CSS path
+     * XPath can be used to navigate through elements 
+     * and attributes in an XML document
+     */
+
     static void Main(string[] args)
     {
         string URLlink = "http://testing.todorvachev.com/selectors/class-name/";
+        string cssPath = "#post-108 > div > figure > img";
+        string xPath = "//*[@id=\"post-108\"]/div/figure/img";
         string className = "testClass";
 
         //Initalise the chrome client
@@ -14,9 +22,11 @@ class ClassName
 
         driver.Navigate().GoToUrl(URLlink);
 
+        //Creates our var called element
+        //Then it finds the elemnt called "ClassName"
         IWebElement element = driver.FindElement(By.ClassName(className));
 
-        //Calling the elements text
+        //Calls the element text
         Console.WriteLine(element.Text);
 
         //Quits the application
