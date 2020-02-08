ce20a5ef9aae36de1c3740318b9d19783aa3e216
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-08 19:19:15 +0000

    Added New Class

---
+++ ClassNamer/Program.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+
+class ClassName
+{
+    static void Main(string[] args)
+    {
+        string URLlink = "http://testing.todorvachev.com/selectors/class-name/";
+        string className = "testClass";
+
+        //Initalise the chrome client
+        IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl(URLlink);
+
+        IWebElement element = driver.FindElement(By.ClassName(className));
+
+        //Calling the elements text
+        Console.WriteLine(element.Text);
+
+        //Quits the application
+        driver.Quit();
+    }
+}
+
