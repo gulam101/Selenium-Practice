dc9c7c8857650447ec68961e25f8e8e303a3d293
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-08 18:54:29 +0000

    Added Methods

--- SeleniumTut/EntryPoint.cs
+++ SeleniumTut/EntryPoint.cs
@@ -1,31 +1,51 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System;
 
 class EntryPoint
   {
      static void Main(string[] args)
      {
+        //URL link
+        string URLlink = "http://google.com";
+
         //Initalise the chrome client
         IWebDriver driver = new ChromeDriver();
 
-        driver.Navigate().GoToUrl("http://google.com");
+        driver.Navigate().GoToUrl(URLlink);
 
         IWebElement element = driver.FindElement(By.Id("lga"));
 
         if(element.Displayed)
         {
-            //Should display this if it works!
-            System.Console.ForegroundColor = System.ConsoleColor.Green;
-            System.Console.WriteLine("Hello World!");
+            GreenMessage("You can see this message!");
         } else
         {
             //Warning message
-            System.Console.ForegroundColor = System.ConsoleColor.Red;
-            System.Console.WriteLine("Well, something went worng!");
+            RedMessage("Well, something went wrong!");
         }
 
         //Quits the application
         driver.Quit();
      }
-  }
+
+    public static void RedMessage(string message)
+    {
+        //Should display this if it works!
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+
+    public static void GreenMessage(string message)
+    {
+        //Incorrect message!
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine(message);
+        Console.ResetColor();
+    }
+
+
+}
 
