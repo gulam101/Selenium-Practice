2799710e8d542960e6789fbc81ea88d7e73c1a52
Gulam Qasim <gfram@hotmail.co.uk>
2020-02-08 16:44:52 +0000

    Added Test Website

--- SeleniumTut/EntryPoint.cs
+++ SeleniumTut/EntryPoint.cs
@@ -1,11 +1,19 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using System.Threading;
 
 class EntryPoint
   {
      static void Main(string[] args)
      {
+        //Initalise the client
         IWebDriver driver = new ChromeDriver();
+
+        driver.Navigate().GoToUrl("www.google.com");
+
+        Thread.Sleep(3000);
+
+        driver.Quit();
      }
   }
 
